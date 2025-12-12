54ec9c755d7722eb125023ce167e3d577c9d1abb
Jhoel <jhoel.sv25@gmail.com>
2025-12-11 19:34:38 -0500

    feat: enhance worker data fetching and add commercial data structure

--- lib/api/worker.ts
+++ lib/api/worker.ts
@@ -1,6 +1,6 @@
-"use server";
+'use server';
 
-import { createClient } from "../supabase/server";
+import { createClient } from '../supabase/server';
 export interface Worker {
   id: string;
   name: string;
@@ -9,11 +9,17 @@ export interface Worker {
 
 export const getWorkers = async (): Promise<Worker[]> => {
   try {
+    const commerciaId = '5595245c-0a3f-46b0-98a4-17a729241be2';
+    const adminId = '92b25d0c-291c-4ad5-9448-85ec62d76d50';
     const supabase = await createClient();
-    const { data, error } = await supabase.from("workers").select("id, name, email");
+    const { data, error } = await supabase
+      .from('workers')
+      .select('id, name, email')
+      .in('role_area_id', [commerciaId, adminId]);
     if (error) throw new Error(error.message);
+    console.log('Workers fetched:', data.length);
     return data;
   } catch (error) {
-    throw new Error("Error fetching workers");
+    throw new Error('Error fetching workers');
   }
 };
